d0a7c57012a19fa4fc65c93c75b674a73727cce5
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-19 14:16:17 -0700

    Update manage.py

--- lets_quiz/manage.py
+++ lets_quiz/manage.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         # exceptions on Python 2.
         try:
             import django
-        except ImportError:
+            except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
